ab8b80f0855624880abedd8d07879307f5f5e929
MarnOng <marnix.ongenae@gmail.com>
2021-07-21 16:15:51 +0200

    +init
    +Initial Commit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)] 
+    [SerializeField] 
+    string storyText;
+
+    [SerializeField]
+    State currentState;
+
+    [SerializeField] 
+    State[] nextStates;
+
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State GetCurrentState()
+    {
+        return currentState;
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
